61f32cf6ce4cf5b2f301ed711d1d7b535969de46
Jérémy Bonhomme <jbonhomme@mirane.com>
2017-12-01 18:25:03 +0100

    maj structure

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ function menuToggle() {
   h.classList.contains( 'is-collapsed' ) ? h.className = 'main-nav' : h.className += ' is-collapsed';
   t.classList.contains( 'is-collapsed' ) ? t.className = 'main-app__toggle ui-button--gvl' : t.className += ' is-collapsed';
 }
+
+function languageToggle() {
+  var l = document.getElementById( 'language' );
+
+  l.classList.contains( 'is-collapsed' ) ? l.className = 'main-app__language' : l.className += ' is-collapsed';
+}
